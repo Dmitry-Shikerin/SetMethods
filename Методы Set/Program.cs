dbd4d2658813c0bc93e9642c3df8f4ebcf98ac63
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-06 14:02:46 +0300

    Добавьте файлы проекта.

---
+++ Методы Set/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Методы_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void CreateRandomNumber()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int СalculateSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+
+}
